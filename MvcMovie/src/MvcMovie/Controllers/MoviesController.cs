6b25647fda46ded57efa496d10bc2cbdc5da21cd
JGroot <gesikamarie@gmail.com>
2016-03-21 00:33:28 -0400

    Adding search for movies

--- MvcMovie/src/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/src/MvcMovie/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Data.Entity;
 using MvcMovie.Models;
+using System.Collections.Generic;
 
 namespace MvcMovie.Controllers
 {
@@ -16,9 +17,26 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public IActionResult Index()
+        public IActionResult Index(string movieGenre, string searchString)
         {
-            return View(_context.Movie.ToList());
+            var GenreQry = from m in _context.Movie
+                           orderby m.Genre
+                           select m.Genre;
+
+            var GenreList = new List<string>();
+            GenreList.AddRange(GenreQry.Distinct());
+            ViewData["movieGenre"] = new SelectList(GenreList);
+
+            var movies = from m in _context.Movie
+                         select m;
+            
+            if (!string.IsNullOrEmpty(searchString))
+                movies = movies.Where(s => s.Title.Contains(searchString));
+
+            if (!string.IsNullOrEmpty(movieGenre))
+                movies = movies.Where(x => x.Genre == movieGenre);
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
